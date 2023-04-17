6a773583026d696ce84f39078fbdccade3052f59
moonhopark <moonho4199@naver.com>
2023-04-17 14:55:56 +0900

    feat: useReducer + useContext

---
+++ src/components/react-basic/UseReducerUseContext/ComponentA.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { CountContext } from '../../../App';
+
+const ComponentA = () => {
+  const countContext = useContext(CountContext);
+  const { countState, countDispatch } = countContext;
+
+  return (
+    <div>
+      ComponentA - {countState}
+      <button onClick={() => countDispatch('increment')}>Increment</button>
+      <button onClick={() => countDispatch('decrement')}>Decrement</button>
+      <button onClick={() => countDispatch('reset')}>Reset</button>
+    </div>
+  );
+};
+
+export default ComponentA;
---
+++ src/components/react-basic/UseReducerUseContext/ComponentF.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { CountContext } from '../../../App';
+
+const ComponentF = () => {
+  const countContext = useContext(CountContext);
+  const { countDispatch } = countContext;
+
+  return (
+    <div>
+      ComponentF
+      <button onClick={() => countDispatch('increment')}>Increment</button>
+      <button onClick={() => countDispatch('decrement')}>Decrement</button>
+      <button onClick={() => countDispatch('reset')}>Reset</button>
+    </div>
+  );
+};
+
+export default ComponentF;
