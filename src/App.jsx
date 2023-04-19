be882734fd239075df15cd5c15be6bf9a545bb49
moonhopark <moonho4199@naver.com>
2023-04-19 15:23:59 +0900

    feat: useCallback 예제 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import UseReducerDataFetchingOne from './components/react-basic/UseReducer/UseReducerDataFetchingOne';
-import UseReducerDataFetchingTwo from './components/react-basic/UseReducer/UseReducerDataFetchingTwo';
+import ParentComponent from './components/react-basic/UseCallback/ParentComponent';
 
 const App = () => {
   return (
     <div className="App">
-      <UseReducerDataFetchingOne />
-      <UseReducerDataFetchingTwo />
+      <ParentComponent />
     </div>
   );
 };
